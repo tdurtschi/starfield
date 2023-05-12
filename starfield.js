432b941477e4c83b493ca93597be25daf5bcb3dc
Teagan Durtschi <teagan@durtschi.com>
2023-05-12 13:36:43 -0500

    change target to outer element

--- starfield.js
+++ starfield.js
@@ -1,4 +1,4 @@
-const FILL_RATIO = 0.8
+const FILL_RATIO = 0.8;
 const starColors = [
     "#FFFFFF",
     "#FFFFFF",
@@ -7,40 +7,51 @@ const starColors = [
     "#777777",
     "#cdbbff",
     "#663267",
-    "#310133"
+    "#310133",
 ];
 
-const generateStarfieldImage = ({fillRatio}) => {
+
+const generateStarfieldImage = ({ fillRatio }) => {
     const canvas = document.createElement("canvas");
     const scale = window.devicePixelRatio;
     canvas.width = Math.floor(1024 * scale);
     canvas.height = Math.floor(768 * scale);
-    const {width, height} = canvas;
-    
+    const { width, height } = canvas;
+
     const ctx = canvas.getContext("2d");
     ctx.scale(scale, scale);
     ctx.translate(0.5, 0.5);
-    
-    for(let i = 0; i < width * (fillRatio ?? FILL_RATIO); i++){
+
+    for (let i = 0; i < width * (fillRatio ?? FILL_RATIO); i++) {
         const color = starColors[Math.floor(Math.random() * starColors.length)]
         ctx.fillStyle = color;
-        
+
         const randX = Math.floor(Math.random() * width);
         const randY = Math.floor(Math.random() * height);
         const size = Math.random() > 0.95
-            ? Math.random() > 0.75 
-                ? 4 : 3 
+            ? Math.random() > 0.75
+                ? 4 : 3
             : 1;
-        
+
         ctx.fillRect(randX, randY, size, size);
     }
-    
+
     const dataUrl = canvas.toDataURL();
     return dataUrl;
 }
 
 export default function Starfield(options) {
-    const {target} = options;
+    const { target } = options;
     const dataUrl = generateStarfieldImage(options);
-    document.getElementById(target).style.cssText = `background: url(${dataUrl}) repeat;`;
+
+    const targetElement = document.getElementById(target);
+    const bgContainer = document.createElement("div");
+    bgContainer.id = "bg-container";
+    targetElement.prepend(bgContainer);
+
+    const bgElement = document.createElement("div");
+    bgElement.id = "bg";
+    bgContainer.prepend(bgElement);
+
+    bgElement.style.cssText = `background: url(${dataUrl}) repeat;`;
 }
