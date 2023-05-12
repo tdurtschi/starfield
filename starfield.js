656bf5caf29d9513308d81bb484cf636c79d07d2
Teagan Durtschi <teagan@durtschi.com>
2023-05-12 13:32:54 -0500

    refactoring, formatting

--- starfield.js
+++ starfield.js
@@ -10,31 +10,37 @@ const starColors = [
     "#310133"
 ];
 
-const canvas = document.createElement("canvas");
-const scale = window.devicePixelRatio;
-canvas.width = Math.floor(1024 * scale);
-canvas.height = Math.floor(768 * scale);
-const {width, height} = canvas;
-
-const ctx = canvas.getContext("2d");
-ctx.scale(scale, scale);
-ctx.fillStyle = "#000000";
-ctx.fillRect(0, 0, width, height);
-ctx.translate(0.5, 0.5);
-
-for(let i = 0; i < width * FILL_RATIO; i++){
-    const color = starColors[Math.floor(Math.random() * starColors.length)]
-    ctx.fillStyle = color;
+const generateStarfieldImage = ({fillRatio}) => {
+    const canvas = document.createElement("canvas");
+    const scale = window.devicePixelRatio;
+    canvas.width = Math.floor(1024 * scale);
+    canvas.height = Math.floor(768 * scale);
+    const {width, height} = canvas;
     
-    const randX = Math.floor(Math.random() * width);
-    const randY = Math.floor(Math.random() * height);
-    const size = Math.random() > 0.95
-        ? Math.random() > 0.75 
-            ? 4 : 3 
-        : 1;
+    const ctx = canvas.getContext("2d");
+    ctx.scale(scale, scale);
+    ctx.translate(0.5, 0.5);
     
-    ctx.fillRect(randX, randY, size, size);
+    for(let i = 0; i < width * (fillRatio ?? FILL_RATIO); i++){
+        const color = starColors[Math.floor(Math.random() * starColors.length)]
+        ctx.fillStyle = color;
+        
+        const randX = Math.floor(Math.random() * width);
+        const randY = Math.floor(Math.random() * height);
+        const size = Math.random() > 0.95
+            ? Math.random() > 0.75 
+                ? 4 : 3 
+            : 1;
+        
+        ctx.fillRect(randX, randY, size, size);
+    }
+    
+    const dataUrl = canvas.toDataURL();
+    return dataUrl;
 }
 
-const dataUrl = canvas.toDataURL();
-document.getElementById("bg").style.cssText = `background: url(${dataUrl}) repeat;`;
+export default function Starfield(options) {
+    const {target} = options;
+    const dataUrl = generateStarfieldImage(options);
+    document.getElementById(target).style.cssText = `background: url(${dataUrl}) repeat;`;
+}
