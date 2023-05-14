b86556540e1dcd7744f21e7a4bf05288e192bd27
Teagan Durtschi <teagan@durtschi.com>
2023-05-14 15:21:52 -0500

    added satellite

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const FILL_RATIO = 0.8;
+let doSatellite;
 const starColors = [
     "#FFFFFF",
     "#FFFFFF",
@@ -48,12 +49,55 @@ const applyBackgroundToTarget = (dataUrl, targetElement) => {
     bgContainer.prepend(bgElement);
 
     bgElement.style.cssText = `background: url(${dataUrl}) repeat;`;
+    return bgElement;
+}
+
+const satellite = (targetElement, initialSpeed, repeatInterval) => () => {
+    let canvas = document.querySelector("#satellite");
+    if (!canvas) {
+        canvas = document.createElement("canvas");
+        canvas.id = "satellite";
+        canvas.width = targetElement.clientWidth;
+        canvas.height = targetElement.clientHeight;
+
+        targetElement.append(canvas);
+    }
+
+    let yPos = Math.floor(Math.random() * (canvas.height - window.innerHeight - 400)) + 200;
+    let xPos = 0;
+    const speed = initialSpeed * window.devicePixelRatio;
+    const direction = (Math.random() * Math.PI / 1.5) - Math.PI / 3;
+    const amtToAddX = speed * Math.cos(direction);
+    const amtToAddY = speed * Math.sin(direction);
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FFFFFF";
+    ctx.translate(0.5, 0.5);
+
+    const updateAnimation = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(xPos, yPos, 2, 2);
+        xPos += amtToAddX;
+        yPos += amtToAddY;
+
+        if (xPos <= canvas.width && yPos <= canvas.height) {
+            requestAnimationFrame(updateAnimation);
+        } else {
+            console.info("Done with Satellite!")
+            setTimeout(() => doSatellite && doSatellite(), repeatInterval);
+        }
+    };
+
+    requestAnimationFrame(updateAnimation);
 }
 
 export default function Starfield(options) {
-    const { target } = options;
+    const { target, satelliteSpeed, satelliteInterval } = options;
     const dataUrl = generateStarfieldImage(options);
 
     const targetElement = document.getElementById(target);
-    applyBackgroundToTarget(dataUrl, targetElement);
+    const backgroundElement = applyBackgroundToTarget(dataUrl, targetElement);
+
+    doSatellite = satellite(backgroundElement, satelliteSpeed, satelliteInterval)
+
+    setTimeout(doSatellite, satelliteInterval ?? 5000);
 }
